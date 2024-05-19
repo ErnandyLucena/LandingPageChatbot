1e12c2c4431e390d03790af0ebda8fb70a5b9f50
Ernandy Lucena <83795827+ErnandyLucena@users.noreply.github.com>
2024-05-18 23:55:10 -0300

    att frontend

--- src/components/BotaoPedido.js
+++ src/components/BotaoPedido.js
@@ -1,24 +1,27 @@
 import React from 'react';
 
-const BotaoPedido = () => {
+const BotaoPedido = ({ sabor, nomeUsuario, horaEnviada, confirmado, id }) => {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center mb-3">
       <button className="bg-white text-black py-2 px-4 rounded-3xl w-11/12 h-24">
-        <div className=' text-black flex items-center'>
-        <img src="/images/pizza-(1).png" alt="Ícone" className="w-9 h-9 ml-4"/>
+        <div className="text-black flex items-center">
+          <img src="/images/pizza-(1).png" alt="Ícone" className="w-9 h-9 ml-4" />
           <div>
-           <h1 className='text-2xl ml-3 items-center'>Calabresa</h1>
+            <h1 className="text-2xl ml-3 items-center">{sabor}</h1>
           </div>
-          <div className='ml-6 linha'>
-          </div>
-          <div className='flex flex-col ml-2 gap-2'>
-            <div className='flex'>
-               <img src="/images/perfil-de-usuario.png" alt="Ícone" className="w-7 h-7 ml-4" />
-              <p className='ml-2 lg:text-sm 2xl:text-base'>Paulo Araujo</p>
+          <div className="ml-6 linha"></div>
+          <div className="flex flex-col ml-2 gap-1">
+            <div className="flex">
+              <img src="/images/perfil-de-usuario.png" alt="Ícone" className="w-6 h-6 ml-4" />
+              <p className="ml-2 lg:text-sm 2xl:text-base">{nomeUsuario}</p>
             </div>
-            <div className='flex'>
-               <img src="/images/endereco.png" alt="Ícone" className="w-6 h-6 ml-5" />
-              <p className='ml-2 lg:text-sm 2xl:text-base'>Inocoop, Rua doze, 62</p>
+            <div className="flex">
+              <img src="/images/endereco.png" alt="Ícone" className="w-5 h-5 ml-5" />
+              <p className="ml-2 lg:text-sm 2xl:text-base">Endereço</p>
+            </div>
+            <div className="flex">
+              <img src="/images/confirmar.png" alt="Ícone" className="w-5 h-5 ml-5" />
+              <p className="ml-2 lg:text-sm 2xl:text-base">{id}</p>
             </div>
           </div>
         </div>
--- src/components/CaixaPedidosCo.js
+++ src/components/CaixaPedidosCo.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './CaixaPedidosCo.css'; // Importa o arquivo CSS para estilização
-import './BotaoPedido'; // Importa o arquivo CSS para estilização
-import BotaoPedido from './BotaoPedido';
+import BotaoPedido from './BotaoPedido'; // Importa o componente BotaoPedido
 
 const CaixaPedidosCo = () => {
+  const [pedidos, setPedidos] = useState([]);
+
+  useEffect(() => {
+    fetchPedidos(); // Carrega os dados da API ao montar o componente
+  }, []);
+
+  const fetchPedidos = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/pedidos');
+      // Ordenar os pedidos do mais recente para o mais antigo
+      const pedidosOrdenados = response.data.reverse();
+      setPedidos(pedidosOrdenados); // Atualiza o estado com os dados recebidos da API
+    } catch (error) {
+      console.error('Erro ao buscar pedidos:', error);
+    }
+  };
+
   return (
     <div className="caixa-pedidos-co">
       <h1 className="text-3xl flex justify-start m-6 text-white">Pedidos</h1>
-      {/* Adicione seus cards de pedido aqui */}
-      <div className=''>
-        <BotaoPedido/>
-        
+      <div>
+        {pedidos.map((pedido) => (
+          <BotaoPedido
+            id={pedido.id} // Certifique-se que 'id' é um campo único no seu pedido
+            sabor={pedido.sabor}
+            nomeUsuario={pedido.nome_usuario}
+            horaEnviada={pedido.hora_enviada}
+            confirmado={pedido.confirmado}
+          />
+        ))}
       </div>
     </div>
   );
